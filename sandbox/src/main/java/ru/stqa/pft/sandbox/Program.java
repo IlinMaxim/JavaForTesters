9d70b631c49e38d35c0357a689eeed55fcb87f94
IlinMaxim <iluin1maxim@gmail.com>
2021-12-12 19:27:53 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 class Program {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
