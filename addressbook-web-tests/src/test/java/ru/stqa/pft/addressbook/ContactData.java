fd7fb8e6c7401415c79dce3f607a563263893a21
IlinMaxim <iluin1maxim@gmail.com>
2021-12-19 02:47:57 +0300

    Добавлен класс ContactCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,79 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String homeTelephone;
+  private final String mobileTelephone;
+  private final String workTelephone;
+  private final String fax;
+  private final String firstEmail;
+
+  public ContactData(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String homeTelephone, String mobileTelephone, String workTelephone, String fax, String firstEmail) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.homeTelephone = homeTelephone;
+    this.mobileTelephone = mobileTelephone;
+    this.workTelephone = workTelephone;
+    this.fax = fax;
+    this.firstEmail = firstEmail;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomeTelephone() {
+    return homeTelephone;
+  }
+
+  public String getMobileTelephone() {
+    return mobileTelephone;
+  }
+
+  public String getWorkTelephone() {
+    return workTelephone;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getFirstEmail() {
+    return firstEmail;
+  }
+}
