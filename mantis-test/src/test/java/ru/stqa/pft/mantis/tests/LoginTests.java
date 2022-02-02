1eaac899870eafb9d323f82286ffac03a1c43202
IlinMaxim <iluin1maxim@gmail.com>
2022-02-02 22:26:06 +0300

    Использую метод skipIfNotFixed в тесте для логина

--- mantis-test/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -3,13 +3,18 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
+import ru.stqa.pft.mantis.model.Issue;
 
+import javax.xml.rpc.ServiceException;
 import java.io.IOException;
 
 public class LoginTests extends TestBase{
 
   @Test
-  public void testLogin() throws IOException {
+  public void testLogin() throws IOException, ServiceException {
+    Issue loginBug = new Issue().withId(1);
+    skipIfNotFixed(loginBug.getId());
+
     HttpSession session = app.newSession();
     Assert.assertTrue(session.login("administrator", "root99"));
     Assert.assertTrue(session.isLoggedInAs("administrator"));
